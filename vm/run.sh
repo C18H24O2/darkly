dd188a3cc5605b3b874c4068688acd08ff7ff96a
xtrm <oss@xtrm.me>
2025-03-15 17:26:33 +0100

    🐛 fix(vm/run.sh): fix virtualbox black screen
    Signed-off-by: xtrm <oss@xtrm.me>

--- vm/run.sh
+++ vm/run.sh
@@ -13,7 +13,7 @@ echo "Running VM"
 VM_NAME="DARKLY_VM"
 VBoxManage unregistervm "$VM_NAME" --delete || true
 VBoxManage createvm --name "$VM_NAME" --register
-VBoxManage modifyvm "$VM_NAME" --memory 2048 --cpus 2 --ostype Debian --nic1 nat
+VBoxManage modifyvm "$VM_NAME" --memory 2048 --cpus 2 --ostype Debian --nic1 nat --vram 64 --graphicscontroller vmsvga
 VBoxManage storagectl "$VM_NAME" --name "IDE Controller" --add ide
 VBoxManage storageattach "$VM_NAME" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "$ISO"
 VBoxManage modifyvm "$VM_NAME" --nic1 bridged --bridgeadapter1 "${1:-eno2}"
